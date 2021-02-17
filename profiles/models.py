f30bae149b002c22dc62a0c76ff91807d75a5dea
Katja Sivertsen <katjasi@stud.ntnu.no>
2021-02-17 12:31:28 +0000

    location with latitude and longitude #7

--- profiles/models.py
+++ profiles/models.py
@@ -7,11 +7,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
-
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=63.446827)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=10.421906)
+    # birth_date = models.DateField(auto_now=False, auto_now_add=False, default=2000-4-9)
     def __str__(self):
         return f'{self.user.username}'
-    # my_location = models.CharField(max_length=20, default="")
-# birth_date = models.DateField(auto_now=False, auto_now_add=False, default=2000-4-9)
-# my_location = models.CharField(max_length=50)
-# If the user gets deleted, then this profile gets deleted to
-# author = models.ForeignKey(User, on_delete=models.CASCADE)

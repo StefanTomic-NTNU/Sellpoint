7caae33ff5b8fd9eabac1477542c5260f6a16f5c
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-02-21 12:18:42 +0100

    correct ad is now displayed when clicking on ad in ads. advertisement_detail.html updated

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from . views import AdvertisementListView, AdvertisementDetailView
 from advertisements import views
 
 
 urlpatterns = [
-    path('', views.ads, name='ads'),
-    path('ad_detail', views.ad_detail, name='ad-detail'),
+    # path('', views.ads, name='ads'),
+    path('', AdvertisementListView.as_view(), name='ads'),
+    # path('ad_detail', views.ad_detail, name='ad-detail'),
+    path('ad/<int:pk>/', AdvertisementDetailView.as_view(), name='ad-detail'),
 ]
 
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Advertisement
 
 
@@ -8,6 +10,18 @@ def ads(request):
     }
     return render(request, 'advertisements/ads.html', context)
 
+#
+# def ad_detail(request):
+#     return render(request, 'advertisements/advertisement_detail.html')
 
-def ad_detail(request):
-    return render(request, 'advertisements/ad_detail.html')
+
+class AdvertisementListView(ListView):
+    model = Advertisement
+    template_name = 'advertisements/ads.html'
+    context_object_name = 'advertisements'
+    ordering = ['-published']    # Forteller at annonsene skal være sortert nyest-eldst publisert'
+    paginate_by = 8     # Nyttig for når vi skal implementere pagination (flere sider med ads)'
+
+
+class AdvertisementDetailView(DetailView):
+    model = Advertisement

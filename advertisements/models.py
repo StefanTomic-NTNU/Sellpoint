d08639d9432d61b1b6d40f8ce28c2c248e43ecc7
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-03-09 11:13:21 +0100

    Added Category to models, migrated db, updated home.html to reflect db. #34

--- advertisements/models.py
+++ advertisements/models.py
@@ -4,15 +4,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    image = models.ImageField(upload_to='images/categories')
+
+    def __str__(self):
+        return self.name
+
+
 class Advertisement(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField()
-    price= models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     image_main = models.ImageField(upload_to='images/advertisements')
     latitude = models.DecimalField(max_digits=9, decimal_places=6, default=63.446827)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, default=10.421906)
     published = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE, related_name='advertisement')
 
     def get_absolute_url(self):
         return reverse('ad-detail', kwargs={'pk': self.pk})

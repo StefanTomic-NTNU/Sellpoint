5c93badc323cff3080d201febe366c9e537f5423
KatjaSi <katja.sivertsen@gmail.com>
2021-03-11 11:19:07 +0100

    Mulighet til å gå fra annonse-siden til profil siden #31

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.views.generic import DetailView
 from .models import Profile
 from .forms import UserRegisterForm

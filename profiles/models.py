e5019ead79b3f45bb18506be5be10849cf749870
Monika <monika-luu@hotmail.com>
2021-03-21 16:14:26 +0100

    #47 Removed unnecessary html tags

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,12 @@
-
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="profile_images/default.jpg", upload_to="profile_images")
+    image = models.ImageField(default="default.jpg", upload_to="profile_images")
     latitude = models.DecimalField(max_digits=9, decimal_places=6, default=63.446827)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, default=10.421906)
 

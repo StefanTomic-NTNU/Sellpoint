d7a695df22612cbc66843d8a18adee1919c2747b
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-04-02 14:39:21 +0200

    Changed styling somewhat

--- contacts/models.py
+++ contacts/models.py
@@ -4,9 +4,11 @@ from datetime import datetime
 class Contact(models.Model):
     recipient_id = models.IntegerField()
     advertisement_id = models.IntegerField(blank=True)
+    advertisement_title = models.CharField(blank=True, max_length=32)
     message = models.TextField(blank=True)
     message_read = models.BooleanField(default=False)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     sender_id = models.IntegerField()
+    sender_name = models.CharField(max_length=64, blank=True)
     def __str__(self):
         return self.message
--- contacts/views.py
+++ contacts/views.py
@@ -10,18 +10,23 @@ from .models import Contact
 
 @login_required
 def contact(request, pk):
-    recipient = Advertisement.objects.get(id=pk).author
+    advertisement = Advertisement.objects.get(id=pk)
+    recipient = advertisement.author
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             advertisement_id = pk
+            advertisement_title = advertisement.title
             recipient_id = recipient.id
             message = form.cleaned_data['message']
             sender_id = request.user.id
+            sender_name = request.user.username
             new_contact = Contact(recipient_id=recipient_id,
                                   advertisement_id=advertisement_id,
+                                  advertisement_title=advertisement_title,
                                   message=message,
-                                  sender_id=sender_id)
+                                  sender_id=sender_id,
+                                  sender_name=sender_name)
             new_contact.save()
             messages.success(request, 'Medling sendt!')
             return redirect('ads')
